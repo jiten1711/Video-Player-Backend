98352b6d1b3035c515d1c3a8ac7ddaf9112a7482
jiten1711 <jitensutarbrjn2000@gmail.com>
2024-01-01 11:39:44 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -10,10 +10,18 @@ app.use(cors({
 }))
 
 // middleware configuration for json, urlencoded and static(images, etc. to be stored in Public folder) files
-app.use(express.json({limit: "16kb", extended: true}))
-app.use(express.urlencoded({limit: "16kb", extended: true}))
+app.use(express.json({ limit: "16kb", extended: true }))
+app.use(express.urlencoded({ limit: "16kb", extended: true }))
 app.use(express.static("public"))
 // there are some files that server stores in cookies of client browser, so we need to parse them
 app.use(cookieParser())
+
+// routes import
+import userRouter from './routes/user.route.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+// the above depicts http://localhost:8000/api/v1/users/<register or login>
+    
 
 export { app }
